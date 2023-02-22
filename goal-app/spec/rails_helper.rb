03fcfa7ade97999b4cb1502ce2c1a25009599e1d
Kaiter Wu <wukaiter@gmail.com>
2023-02-22 15:18:02 -0500

    finishing first validations

--- goal-app/spec/rails_helper.rb
+++ goal-app/spec/rails_helper.rb
@@ -1,10 +1,10 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require "spec_helper"
-require "shoulda-matchers"
-require "rspec/rails"
-require "capybara/rspec"
+require 'spec_helper'
+require 'shoulda-matchers'
+require 'capybara/rspec'
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
+
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
@@ -41,6 +41,7 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+  config.include FactoryBot::Syntax::Methods
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
@@ -63,4 +64,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
